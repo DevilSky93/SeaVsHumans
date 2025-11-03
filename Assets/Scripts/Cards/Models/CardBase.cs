ae9d9d542c66b11b3abc30cf5c571c3e3bb75969
MaximeVg <49032976+DevilSky93@users.noreply.github.com>
2025-11-03 23:02:33 +0100

    Hover card in hand animation

--- Assets/Scripts/Cards/Models/CardBase.cs
+++ Assets/Scripts/Cards/Models/CardBase.cs
@@ -1,15 +1,30 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Cards.Models
 {
-    public abstract class CardBase : MonoBehaviour
+    public class CardBase : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         [SerializeField] private CardData cardData;
         private Card _card;
+        private float _originalYPosition;
 
         private void Awake()
         {
             _card = new Card(cardData);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _originalYPosition = transform.position.y;
+            transform.DOMoveY(transform.position.y + 0.2f, .25f);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            transform.DOKill();
+            transform.DOMoveY(_originalYPosition, .01f);
         }
     }
 }
